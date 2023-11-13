d991d0f52e040630f0fd21e80dcabd68f70d4498
ismoilovsalohiddin <ismailovsalohiddin8@gmail.com>
2023-11-13 18:05:41 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,161 @@
+
+let users = [
+  {
+      name: "Nurmuhammad",
+      age: 22,
+      position: "mentor",
+      gender: "male"
+  },
+
+  {
+      name: "Sardor",
+      age: 20,
+      position: "asistent",
+      gender: "male"
+  },
+
+  {
+      name: "Muhammadrasul",
+      age: 18,
+      position: "o'quvchi",
+      gender: "male"
+  },
+
+  {
+      name: "Ohunjon",
+      age: 20,
+      position: "o'quvchi",
+      gender: "male"
+  },
+
+  {
+      name: "Yasharbek",
+      age: 20,
+      position: "o'quvchi",
+      gender: "male"
+  },
+
+  {
+      name: "Hasanali",
+      age: 21,
+      position: "o'quvchi",
+      gender: "male"
+  },
+
+  {
+      name: "Usmon",
+      age: 18,
+      position: "o'quvchi",
+      gender: "male"
+  },
+
+  {
+      name: "Salohiddin",
+      age: 18,
+      position: "o'quvchi",
+      gender: "male"
+  }
+]
+
+let user1Name = document.querySelector(".user1__name");
+user1Name.textContent = ("name:"+users[0].name)
+
+let user1Age = document.querySelector(".user1__age");
+user1Age.textContent = ("age:"+users[0].age)
+
+let user1Position = document.querySelector(".user1__position");
+user1Position.textContent = ("position:"+users[0].position)
+
+let user1Gender = document.querySelector(".user1__gender");
+user1Gender.textContent = ("gender:"+users[0].gender)
+
+
+let user2Name = document.querySelector(".user2__name");
+user2Name.textContent = ("name:"+users[2].name)
+
+let user2Age = document.querySelector(".user2__age");
+user2Age.textContent = ("age:"+users[2].age)
+
+let user2Position = document.querySelector(".user2__position");
+user2Position.textContent = ("position:"+users[2].position)
+
+let user2Gender = document.querySelector(".user2__gender");
+user2Gender.textContent = ("gender:"+users[2].gender)
+
+
+let user3Name = document.querySelector(".user3__name");
+user3Name.textContent = ("name:"+users[3].name)
+
+let user3Age = document.querySelector(".user3__age");
+user3Age.textContent = ("age:"+users[3].age)
+
+let user3Position = document.querySelector(".user3__position");
+user3Position.textContent = ("position:"+users[3].position)
+
+let user3Gender = document.querySelector(".user3__gender");
+user3Gender.textContent = ("gender:"+users[3].gender)
+
+
+let user4Name = document.querySelector(".user4__name");
+user4Name.textContent = ("name:"+users[4].name)
+
+let user4Age = document.querySelector(".user4__age");
+user4Age.textContent = ("age:"+users[4].age)
+
+let user4Position = document.querySelector(".user4__position");
+user4Position.textContent = ("position:"+users[4].position)
+
+let user4Gender = document.querySelector(".user4__gender");
+user4Gender.textContent = ("gender:"+users[4].gender)
+
+
+let user5Name = document.querySelector(".user5__name");
+user5Name.textContent = ("name:"+users[5].name)
+
+let user5Age = document.querySelector(".user5__age");
+user5Age.textContent = ("age:"+users[5].age)
+
+let user5Position = document.querySelector(".user5__position");
+user5Position.textContent = ("position:"+users[5].position)
+
+let user5Gender = document.querySelector(".user5__gender");
+user5Gender.textContent = ("gender:"+users[5].gender)
+
+
+let user6Name = document.querySelector(".user6__name");
+user6Name.textContent = ("name:"+users[6].name)
+
+let user6Age = document.querySelector(".user6__age");
+user6Age.textContent = ("age:"+users[6].age)
+
+let user6Position = document.querySelector(".user6__position");
+user6Position.textContent = ("position:"+users[6].position)
+
+let user6Gender = document.querySelector(".user6__gender");
+user6Gender.textContent = ("gender:"+users[6].gender)
+
+
+let user7Name = document.querySelector(".user7__name");
+user7Name.textContent = ("name:"+users[7].name)
+
+let user7Age = document.querySelector(".user7__age");
+user7Age.textContent = ("age:"+users[7].age)
+
+let user7Position = document.querySelector(".user7__position");
+user7Position.textContent = ("position:"+users[7].position)
+
+let user7Gender = document.querySelector(".user7__gender");
+user7Gender.textContent = ("gender:"+users[7].gender)
+
+
+let user8Name = document.querySelector(".user8__name");
+user8Name.textContent = ("name:"+users[8].name)
+
+let user8Age = document.querySelector(".user8__age");
+user8Age.textContent = ("age:"+users[8].age)
+
+let user8Position = document.querySelector(".user8__position");
+user8Position.textContent = ("position:"+users[8].position)
+
+let user8Gender = document.querySelector(".user8__gender");
+user8Gender.textContent = ("gender:"+users[8].gender)
